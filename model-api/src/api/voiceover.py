e91bc46d2750a3a07d38c3ab7347713a56bf296d
Jtolen543 <Jtolen543@gmail.com>
2025-06-28 18:56:16 -0400

    pipeline changes

--- model-api/src/api/voiceover.py
+++ model-api/src/api/voiceover.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, UploadFile, File, Form
+from fastapi import APIRouter, Form
 from fastapi.responses import JSONResponse
 import os
 from config import settings
@@ -6,10 +6,11 @@ from core.voiceover import generate_voiceover
 
 router = APIRouter()
 
-@router.post("/voiceover/")
+@router.post('')
 def voiceover(text: str = Form(...), voice: str = Form("default")):
     try:
-        output_audio_path = generate_voiceover(text, voice)
+        output_path = os.path.join(settings.DOWNLOAD_FOLDER, "audios", "voiceover.mp3")
+        output_audio_path = generate_voiceover(text, voice, output_path)
         return JSONResponse({"voiceover_audio": output_audio_path})
     except Exception as e:
         return JSONResponse({"error": str(e)})
--- model-api/src/main.py
+++ model-api/src/main.py
@@ -1,28 +1,26 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import JSONResponse
-from config import settings
-from celery_app.app import celery
+from fastapi.responses import PlainTextResponse, JSONResponse
 from api.router import router as api_router
 from websocket.router import router as ws_router
 
 app = FastAPI()
 app.add_middleware(
-    CORSMiddleware,
-    allow_origins=[
-      "https://fragsai.com", 
-      "https://www.fragsai.com", 
-      "https://www.model.fragsai.com", 
-      "https://model.fragsai.com", 
-      "https://www.backend.fragsai.com",
-      "https://backend.fragsai.com",
-      "http://localhost:5173.com",
-      "http://localhost:8000.com",
-      "http://localhost:3000.com",
-    ],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+  CORSMiddleware,
+  allow_origins=[
+    "https://fragsai.com", 
+    "https://www.fragsai.com", 
+    "https://www.model.fragsai.com", 
+    "https://model.fragsai.com", 
+    "https://www.backend.fragsai.com",
+    "https://backend.fragsai.com",
+    "http://localhost:5173.com",
+    "http://localhost:8000.com",
+    "http://localhost:3000.com",
+  ],
+  allow_credentials=True,
+  allow_methods=["*"],
+  allow_headers=["*"],
 )
 
 # Mount routes
@@ -31,4 +29,4 @@ app.include_router(ws_router, prefix="/ws")
 
 @app.get("/")
 async def server_status():
-    return JSONResponse({"message": "LLM server is active and running"})
+  return JSONResponse({"message": "Models are ready for usage"}, 200)
